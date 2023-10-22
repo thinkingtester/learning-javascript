837a1510e1dd52aace2421cae2395dfabb676b3c
Kristin Jackvony <kristinjackvony@Kristins-MacBook-Pro.local>
2023-10-22 16:39:20 -0400

    Adding in variables.js

---
+++ variables.js
@@ -0,0 +1,21 @@
+// NUMBERS 
+
+let x = 5
+console.log(x)
+
+let y = 5.5
+console.log(y)
+
+// STRINGS
+
+let z = "hello"
+console.log(z)
+
+let a = "A string can be a whole sentence"
+console.log(a)
+
+// BOOLEAN
+
+let b = true
+console.log(b)
+
