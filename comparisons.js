1b16635d935c3b7ace7550c8288bb24f2009e3ae
Kristin Jackvony <kristinjackvony@Kristins-MacBook-Pro.local>
2023-11-12 11:48:56 -0500

    Adding comparisons

---
+++ comparisons.js
@@ -0,0 +1,17 @@
+console.log("Does 5 equal 4? " + (5 == 4))
+console.log("Does 5 equal 5? " + (5 == 5))
+console.log("Does 4 not equal 5? " + (4 != 5))
+
+console.log("Is 5 greater than 4? " + (5 > 4))
+console.log("Is 5 less than 4? " + (5 < 4))
+console.log("Is 4 less than or equal to 5? " + (4 <= 5))
+console.log("Is 5 greater than or equal to 5? " + (5 >= 5)) 
+
+let x = 3
+let y = 2
+
+console.log("Is x greater than y? " + (x > y))
+console.log("Is x equal to y? " + (x == y))
+
+// Don't do this!  One equal sign reassigns the variable value
+console.log (x = y)
--- variables.js
+++ variables.js
@@ -54,3 +54,18 @@ console.log(k)
 e = e + 1
 console.log(e)
 
+// CONCATENATING STRINGS
+
+let firstWord = "Hello"
+let secondWord = "World!"
+
+let sentence = firstWord + secondWord
+console.log(sentence)
+
+sentence = firstWord + " " + secondWord
+console.log(sentence)
+
+console.log("The value of x is " + x)
+console.log("The value of a is " + a)
+
+console.log("The value of e + f is " + (e + f))
