bd7de3a28eb269936384aaff53207f2ffa2adf5f
Kristin Jackvony <kristinjackvony@Kristins-MacBook-Pro.local>
2023-12-04 21:11:27 -0500

    Adding loops file

---
+++ loops.js
@@ -0,0 +1,16 @@
+// For Loop
+
+// First part- creates a counter variable
+// Second part- sets the condition in which the loop will execute
+// Third part- increments the counter variable
+
+for(x = 1; x < 5; x++) {
+    console.log("x is " + x)
+}
+
+let y = "foo"
+
+for(x = 1; x < 5; x++) {
+    y = y + "bar"
+    console.log(y)
+}
